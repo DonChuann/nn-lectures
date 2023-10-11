d34ebda1376dcb0e25cfddd1a28f4c60dd9c87f4
DonChuan <friedrichdalitz@gmx.de>
2023-10-11 15:04:20 +0200

    completed implementation of transformer model

--- nanogpt.py
+++ nanogpt.py
@@ -3,14 +3,18 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 4 # how many independant sequences will be processed in parallel?
-block_size  = 8 # what is the maximum context length for predictions?
+batch_size = 32 # how many independant sequences will be processed in parallel?
+block_size  = 64 # what is the maximum context length for predictions?
 max_iters = 10000
-eval_interval = 300
-learning_rate = 1e-3
+eval_interval = 500
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f"using device: {device}")
 eval_iters = 200
-n_embd = 32
+n_embd = 128
+dropout = 0.2
+n_layer = 8
+n_head = 8
 #-----------------
 
 
@@ -59,6 +63,22 @@ def estimate_loss():
     model.train()
     return out
 
+class MultiHeadAttention(nn.Module):
+    #multiple heads of self-attention in parallel
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        return out
+
+
+
 class Head(nn.Module):
     #one head of self attention
 
@@ -68,6 +88,8 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B,T,C = x.shape
@@ -79,10 +101,44 @@ class Head(nn.Module):
         wei = q @ k.transpose(-1,-2) / (C ** 0.5)
         wei = wei.masked_fill(self.tril[:T,:T] == 0, float('-inf'))
         wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
         #apply attention to values
         x = wei @ v
         return x
+    
+class FeedForward(nn.Module):
+    # a simple linear layer followed by a non linearity
 
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(n_embd * 4, n_embd),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+    
+
+class Block(nn.Module):
+    # a transformer block
+
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
 
 class BigramLanguageModel(nn.Module):
     def __init__(self):
@@ -90,7 +146,9 @@ class BigramLanguageModel(nn.Module):
         #each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)]
+        )
+        self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -99,7 +157,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B, T, C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
         x = tok_emb + pos_emb # (B, T, C)
-        x = self.sa_head(x)
+        x = self.blocks(x)
         logits = self.lm_head(x)
 
         if targets is None:
@@ -137,7 +195,6 @@ optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
 
 
 for iter in range(max_iters):
-
     # every once in a while evaluate the loss on train and val sets
     if iter % eval_interval == 0:
         losses = estimate_loss()
@@ -155,7 +212,7 @@ for iter in range(max_iters):
 
 #generate from the model
 context = torch.zeros((1,1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=100)[0].tolist()))
-
+print(decode(m.generate(context, max_new_tokens=1000)[0].tolist()))
+print(f"final train loss {losses['train']:.4f}, final val loss {losses['val']:.4f}")
 
 
